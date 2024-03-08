8cfbc32751dae24554a55a4409214ffaf0544838
fkdads <fabikneer@gmail.com>
2024-03-08 12:07:42 +0100

    Include second sampling step: - split subtitle_placement implementation

--- dataset/helper/dataset_sampler_helper.py
+++ dataset/helper/dataset_sampler_helper.py
@@ -19,8 +19,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import argparse
-# import sys
 
+
+# import sys
 
 def __init_arg_parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Provide input parameters")
@@ -245,6 +246,284 @@ def format_frames(frame, output_size):
     # return frame
 
 
+class final_sampling:
+    @staticmethod
+    def run(args):
+        # Example usage:
+        path_jsons = r'G:\Coding\VLM_subtitling\dataset_labeled'
+        path_dataset = r'G:\Coding\VLM_subtitling\dataset_processed'
+        output_path = "\\".join(args.video_path.split("\\")[:-1]) + r"\dataset_final"
+        final_sampling.filter_dataset(path_jsons, path_dataset, output_path, ignore_different=True)
+
+    @staticmethod
+    def process_files(root):
+        root, dirs, files = root[0], root[1], root[2]
+
+        copy_dict = {}
+        image_height = -100
+        image_width = -100
+        files = []
+
+        if root.split("\\")[-2] == "A" or root.split("\\")[-2] == "B" or root.split("\\")[-2] == "_A":
+            for file in files:
+                if ".png" in file or ".jpg" in file or ".jpeg" in file and image_width == -100 or image_height == -100:
+                    image_width, image_height = get_image_dimensions(os.path.join(root, file))
+
+                if root.split("\\")[-2].upper() == "A":
+                    if "-".join(file.split("-")[:1]) in data_dict[
+                        [folder_name for folder_name in os.listdir(path_dataset) if
+                         folder_name.endswith("single")][0]][os.path.basename(root)]:
+                        copy_dict[os.path.join(root, file)] = [
+                            "\\subtitle_placement\\single_default\\Pix2Pix\\A\\" + os.path.basename(
+                                root) + "\\" + file,
+                            "\\subtitle_placement\\single_default\\SAN\\A\\" + os.path.basename(
+                                root) + "\\" + file, [(1024, 1024),
+                                                      "\\subtitle_placement\\single_default\\DALL-E\\" + os.path.basename(
+                                                          root) + "\\" + file]]
+
+                if root.split("\\")[-2].upper() == "_A":
+                    if "-".join(file.split("-")[:1]) in data_dict[
+                        [folder_name for folder_name in os.listdir(path_dataset) if "single" in folder_name][
+                            0]][os.path.basename(root)]:
+                        copy_dict[os.path.join(root, file)] = [
+                            "\\subtitle_placement\\single_empty\\Pix2Pix\\A\\" + os.path.basename(
+                                root) + "\\" + file,
+                            "\\subtitle_placement\\single_empty\\SAN\\A\\" + os.path.basename(
+                                root) + "\\" + file, [(1024, 1024),
+                                                      "\\subtitle_placement\\single_empty\\DALL-E\\A\\" + os.path.basename(
+                                                          root) + "\\" + file]]
+
+                elif root.split("\\")[-2].upper() == "B":
+                    if "-".join(file.split("-")[:1]) in data_dict[
+                        [folder_name for folder_name in os.listdir(path_dataset) if "single" in folder_name][
+                            0]][os.path.basename(root)]:
+                        copy_dict[os.path.join(root, file)] = [
+                            "\\subtitle_placement\\single_default\\Pix2Pix\\B\\" + os.path.basename(
+                                root) + "\\" + file,
+                            "\\subtitle_placement\\single_empty\\Pix2Pix\\B\\" + os.path.basename(
+                                root) + "\\" + file]
+
+                elif root.split("\\")[-2].upper() == "PIXELMAPS":
+                    if "-".join(file.split("-")[:1]) in data_dict[
+                        [folder_name for folder_name in os.listdir(path_dataset) if "single" in folder_name][
+                            0]][os.path.basename(root)]:
+                        copy_dict[os.path.join(root, file)] = [
+                            "\\subtitle_placement\\single_default\\SAN\\pixelmaps\\" + os.path.basename(
+                                root) + "\\" + file,
+                            "\\subtitle_placement\\single_empty\\SAN\\pixelmaps\\" + os.path.basename(
+                                root) + "\\" + file]
+
+                elif root.split("\\")[-2].upper() == "JSONS":
+                    if "-".join(file.split("-")[:1]) in data_dict[
+                        [folder_name for folder_name in os.listdir(path_dataset) if "single" in folder_name][
+                            0]][os.path.basename(root)]:
+                        copy_dict[os.path.join(root, file)] = [
+                            "\\subtitle_placement\\single_default\\SAN\\pixelmaps\\" + os.path.basename(
+                                root) + "\\" + file,
+                            "\\subtitle_placement\\single_empty\\SAN\\pixelmaps\\" + os.path.basename(
+                                root) + "\\" + file]
+
+            generated_image = generate_dall_e_mask(image_height=image_height, image_width=image_width)
+            if "to_create" in copy_dict:
+                copy_dict["to_create"].append(
+                    [generated_image, "\\subtitle_placement\\single_empty\\DALL-E\\mask.png"])
+            else:
+                copy_dict["to_create"] = [
+                    [generated_image, "\\subtitle_placement\\single_empty\\DALL-E\\mask.png"]]
+            generated_image = generate_dall_e_mask(size=0, image_height=image_height, image_width=image_width)
+            if "to_create" in copy_dict:
+                copy_dict["to_create"].append(
+                    [generated_image, "\\subtitle_placement\\single_default\\DALL-E\\mask.png"])
+            else:
+                copy_dict["to_create"] = [[generated_image,
+                                           "\\subtitle_placement\\single_default\\DALL-E\\mask.png"]]  # if dirs in data_dict:  #     # Create folder in output_path if it doesn't exist  #     output_folder = os.path.join(output_path, folder_name)  #     if not os.path.exists(output_folder):  #         os.makedirs(output_folder)  #  #     # Iterate over files in the subfolder  #     for file_name in os.listdir(os.path.join(path_dataset, folder_name)):  #         # Perform filtering based on data in data_dict  #         filtered_data = [data for data in data_dict[folder_name] if data[  #             'filter_criteria'] == file_name]  # Modify the filter_criteria as per your data  #         # Write filtered data to output_path  #         with open(os.path.join(output_folder, file_name), 'w') as output_file:  #             json.dump(filtered_data, output_file)
+
+        return copy_dict
+
+    @staticmethod
+    def generate_dall_e_mask(size: int = 30, image_height: int = 1024, image_width: int = 1024):
+        from PIL import Image, ImageDraw
+        # Create a new image with the desired size and white background
+        image_size = (image_width, image_height)
+        mask_image = Image.new("RGBA", image_size, color=(255, 255, 255, 255))
+
+        # Create a mask with the same size
+        mask = Image.new("L", image_size, 0)
+
+        # Draw a shape on the mask to define the area you want to make transparent
+        draw = ImageDraw.Draw(mask)
+        draw.rectangle([(round((image_width / 2) - (size / 2)), round(image_height * 0.9 - (size / 2))),
+                        (round((image_width / 2) + (size / 2)), round(image_height * 0.9 + (size / 2)))],
+                       fill=255)  # Adjust coordinates as needed
+
+        # Apply the mask to the image
+        mask_image.putalpha(mask)
+
+        return mask_image
+
+
+    @staticmethod
+    def generate_dall_e_mask(size: int = 30, image_height: int = 1024, image_width: int = 1024):
+        # Create a new image with the desired size and white background
+        image_size = (image_width, image_height)
+        mask_image = Image.new("RGBA", image_size, color=(255, 255, 255, 255))
+
+        # Create a mask with the same size
+        mask = Image.new("L", image_size, 0)
+
+        # Draw a shape on the mask to define the area you want to make transparent
+        draw = ImageDraw.Draw(mask)
+        draw.rectangle([(round((image_width / 2) - (size / 2)), round(image_height * 0.9 - (size / 2))),
+                        (round((image_width / 2) + (size / 2)), round(image_height * 0.9 + (size / 2)))],
+                       fill=255)  # Adjust coordinates as needed
+
+        # Apply the mask to the image
+        mask_image.putalpha(mask)
+
+        return mask_image
+
+
+    @staticmethod
+    def get_image_dimensions(image_path):
+        from PIL import Image
+        try:
+            with Image.open(image_path) as img:
+                width, height = img.size
+                return width, height
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print("An error occurred:", e)
+
+
+    @staticmethod
+    def read_in_json_files(path_jsons: str):
+        data_dict = {}
+        for root, dirs, files in os.walk(path_jsons):
+            base_folder = root.replace(path_jsons, "").replace(os.path.basename(root), "").strip("\\")
+            for file in files:
+                if file.endswith('.json'):
+                    folder_name = os.path.basename(root)
+                    with open(os.path.join(root, file), 'r') as json_file:
+                        data = json.load(json_file)
+                        data = ["-".join(el["file_name"].split("-")[1:]) for el in data["images"]]
+                        if base_folder not in data_dict:
+                            data_dict[base_folder] = {}
+
+                        if folder_name not in data_dict[base_folder]:
+                            data_dict[base_folder][folder_name] = []
+                        data_dict[base_folder][folder_name].extend(data)
+
+        return data_dict
+
+
+    @staticmethod
+    def rebalance_annotated_datasaet(data_dict):
+        # data_dict_temp = copy.deepcopy(data_dict)
+        test = 0
+        train = 0
+        val = 0
+        sum_instances = - 1
+        for pkey, pvalue in data_dict.items():
+            final_files_list = []
+            try:
+                if "test" in data_dict[pkey]:
+                    test = len(data_dict[pkey]["test"])
+                    final_files_list.extend(data_dict[pkey]["test"])
+                else:
+                    test = 0
+                if "train" in data_dict[pkey]:
+                    train = len(data_dict[pkey]["train"])
+                    final_files_list.extend(data_dict[pkey]["train"])
+                else:
+                    train = 0
+                if "val" in data_dict[pkey]:
+                    val = len(data_dict[pkey]["val"])
+                    final_files_list.extend(data_dict[pkey]["val"])
+                else:
+                    val = 0
+            except:
+                raise ValueError("Unexpected data structure found")
+
+            if sum_instances > 0:
+                if ignore_different is False:
+                    assert sum_instances == test + train + val, (f"You have sorted out different frames in the "
+                                                                 f"respective datasets during manual annotation. "
+                                                                 f"This results in different data records. "
+                                                                 f"Please set the parameter ignore_different to "
+                                                                 f"True to suppress this error and to sample based "
+                                                                 f"on active speaker detection for single "
+                                                                 f"experiment. Key {pkey} differs "
+                                                                 f"in {abs(sum_instances - (test + train + val))}")
+
+            sum_instances = test + train + val
+            if train > 0:
+                train_rebalance = int(sum_instances * rebalance[0])
+            else:
+                train_rebalance = 0
+            if val > 0:
+                val_rebalance = int(sum_instances * rebalance[1])
+            else:
+                val_rebalance = 0
+
+            if sum_instances == test:
+                test_rebalance = test
+            else:
+                test_rebalance = int(sum_instances * rebalance[2])
+
+            assert train_rebalance + val_rebalance + test_rebalance <= sum_instances, (
+                "Logic issue, please fix "
+                "code.")
+
+            train_images = sorted(final_files_list[:train_rebalance + val_rebalance + test_rebalance])[
+                           :train_rebalance]
+            val_images = sorted(final_files_list[:train_rebalance + val_rebalance + test_rebalance])[
+                         train_rebalance:train_rebalance + val_rebalance]
+            test_images = sorted(final_files_list[:train_rebalance + val_rebalance + test_rebalance])[
+                          train_rebalance + val_rebalance:]
+
+            data_dict[pkey]["test"] = test_images
+            data_dict[pkey]["val"] = val_images
+            data_dict[pkey]["train"] = train_images
+
+
+    @staticmethod
+    def filter_dataset(path_jsons, path_dataset, output_path, rebalance: list = [0.75, 0.15, 0.1],
+                       ignore_different: bool = False):
+        from PIL import Image, ImageDraw
+
+        import os
+        import json  # , copy
+        from multiprocessing import Pool
+        assert len(rebalance) == 3, "The list should have exactly three values."
+        assert sum(rebalance) == 1, "The sum of the values in the list should equal 1."
+
+        # Create a dictionary to store data based on folder names
+        data_dict = final_sampling.read_in_json_files(path_jsons)
+        final_sampling.rebalance_annotated_datasaet(data_dict)
+
+        # Iterate over subfolders in path_dataset
+        assert any("single" in folder_name for folder_name in os.listdir(path_dataset)) and any(
+            "overlapped" in folder_name for folder_name in os.listdir(path_dataset)) and any(
+            "voting" in folder_name for folder_name in os.listdir(path_dataset)), (
+            "Please follow the instructions in the README.md file. "
+            "Provide three separate folders for single, overlapping, and voting experiment. Ensure it is the "
+            "correct path")
+
+        root_dirs = os.walk(os.path.join(path_dataset, [folder_name for folder_name in os.listdir(path_dataset) if
+                                                        "single" in folder_name][0]))
+
+        with Pool() as pool:
+            results = pool.map(process_files, root_dirs)
+
+        # Now you can collect and aggregate the results
+        final_result_dict = {}
+        for result in results:
+            final_result_dict.update(result)
+
+
+
+
 class SubtitlePlacement:
     @staticmethod
     def _check_args_init(args: argparse.Namespace) -> None:
@@ -385,7 +664,7 @@ class SubtitlePlacement:
         assert isinstance(self.n_frames, int) and self.n_frames > 0
         assert isinstance(self.frame_step, int) and self.frame_step >= 0
         assert (self.overlay_frames <= 1 and self.frames_per_step >= 1) or (
-                    self.overlay_frames >= 1 and self.frames_per_step <= 1), "Only overlay ofr frames_per_step can be used. Not both at the same time."
+                self.overlay_frames >= 1 and self.frames_per_step <= 1), "Only overlay ofr frames_per_step can be used. Not both at the same time."
 
         self.__initialize_output_directories()
         self.initialized = True
@@ -409,7 +688,7 @@ class SubtitlePlacement:
 
         cap = cv2.VideoCapture(video_path)
 
-        #if cap.isOpened() is False:
+        # if cap.isOpened() is False:
         #    cap = cv2.VideoCapture(video_path)
 
         assert cap.isOpened() is True, "Error reading video file"
@@ -568,7 +847,7 @@ class SubtitlePlacement:
 
                     if self.overlay_frames > 1 or (
                             self.frames_per_step > 1 and temp_frames_per_step == statistics.median(
-                            list(range(1, self.frames_per_step + 1)))):
+                        list(range(1, self.frames_per_step + 1)))):
                         if self.overlay_frames > 1:
                             frames_face_recognition.append(frame_original.copy())
                         else:
@@ -1089,9 +1368,10 @@ class GLIP_coco_like:
         cap.set(cv2.CAP_PROP_POS_FRAMES, start)
 
         name_fixed = str(pathlib.Path().resolve().joinpath("dataset_processed", re.sub(r" +", "_",
-                                                                          video_path.split("/")[-1].split(".")[
-                                                                              0].replace(
-                                                                              "-", ""))))
+                                                                                       video_path.split("/")[-1].split(
+                                                                                           ".")[
+                                                                                           0].replace(
+                                                                                           "-", ""))))
         if test:
             name_fixed = name_fixed.replace("_train", "_test")
         if val:
@@ -1738,8 +2018,9 @@ def create_input_data(video_path: str, subtitles_array: np.ndarray, used_start_p
     else:
         final_list_captions = []
     name_fixed = str(pathlib.Path().resolve().joinpath("dataset_processed", re.sub(r" +", "_",
-                                                                      video_path.split("/")[-1].split(".")[0].replace(
-                                                                          "-", ""))))
+                                                                                   video_path.split("/")[-1].split(".")[
+                                                                                       0].replace(
+                                                                                       "-", ""))))
     # if output_frames_instructions_middle_positioned
 
     if position_subtitle_middle and output_frames_instructions_middle_positioned is False and output_box is False:
